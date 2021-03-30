db696a904420c79989c9ab0a18b0e5c4a3b68dc7
Alex Foshay <FoshAlex157@GoTVDSB.ca>
2021-03-30 10:35:34 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName;
+            string lastName;
+
+
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student("Alex", "Foshay"));
+            students.Add(new Student("Evan", "Gurtz"));
+            students.Add(new Student("John", "Doe"));
+
+
+
+
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            do
+            {
+                Console.WriteLine("Enter the first name of a student you want to add.");
+                firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter the last name of the student you want to add.");
+                lastName = Console.ReadLine();
+
+                students.Add(new Student(firstName, lastName));
+
+            } while (students.Count < 5);
+
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.StudentNumber + " " + student.Email);
+
+            }
+
+            students.Sort();
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_Classes
+{
+#pragma warning disable CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
+    internal class Student : IComparable<Student>
+#pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
+    {
+        private static Random generator = new Random();
+        private string firstName;
+        private string lastName;
+        private int studentNum;
+        private string email;
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName.Trim();
+            this.lastName = lastName.Trim();
+            studentNum = generator.Next(999) + 392000;
+            GenerateEmail();
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                this.firstName = value;
+                GenerateEmail();
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                this.lastName = value;
+                GenerateEmail();
+            }
+        }
+
+        public int StudentNumber
+        {
+            get
+            {
+                return studentNum;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+        }
+
+
+        public void ResetStudentNumber()
+        {
+            this.studentNum = generator.Next(999) + 392000;
+            GenerateEmail();
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Student student = obj as Student;   //Student comparison
+            if (student == null)
+                return false;
+            return this.FirstName == student.FirstName && this.LastName == student.LastName && this.StudentNumber == student.StudentNumber;
+        }
+
+
+        public int CompareTo(Student that)
+        {
+            if (this.LastName.CompareTo(that.LastName) == 0) //If last names are = , compare the first names
+                return this.StudentNumber.CompareTo(that.StudentNumber);
+
+            return this.LastName.CompareTo(that.LastName);  //compare the last names
+        }
+
+        private void GenerateEmail()
+        {
+            string firstThree, lastThree;
+
+            if (firstName.Length <= 3)
+            {
+                firstThree = firstName.ToLower();
+            }
+            else
+            {
+                firstThree = firstName.Substring(0, 3).ToLower();
+            }
+
+            if (lastName.Length <= 3)
+            {
+                lastThree = lastName.ToLower();
+            }
+            else
+            {
+                lastThree = lastName.Substring(0, 3).ToLower();
+            }
+
+
+            email = firstThree + lastThree + (StudentNumber + "").Substring(3) + "@ICS4U1.com";
+
+
+        }
+
+    }
+
+
+
+
+
+}
